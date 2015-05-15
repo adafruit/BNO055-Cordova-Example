3921e3d86322bdd1228a012dc9df1a8d22071af1
Todd Treece <toddtreece@gmail.com>
2015-05-15 18:59:55 -0400

    adds cordova cli install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+npm install -g cordova
 cordova plugin add cordova-plugin-whitelist@1
 cordova plugin add com.megster.cordova.bluetoothserial
 cordova plugin add https://github.com/apache/cordova-plugin-splashscreen.git
