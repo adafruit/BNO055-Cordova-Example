f4afe3461bf7653d0a25cc1e3ea7c748c9e36a57
Todd Treece <toddtreece@gmail.com>
2015-05-15 18:46:32 -0400

    adds update arduino sketch

--- arduino_ble/arduino_ble.ino
+++ arduino_ble/arduino_ble.ino
@@ -1,71 +1,57 @@
 #include <Wire.h>
 #include <SPI.h>
+#include <SoftwareSerial.h>
+
 #include "Adafruit_Sensor.h"
 #include "Adafruit_BNO055.h"
 #include "Adafruit_BLE.h"
-#include "Adafruit_BLE_SWUART.h"
+#include "Adafruit_BluefruitLE_UART.h"
 
 #define RTS  11
 #define RX    6
 #define TX   10
 #define CTS   9
-#define MODE 12
+#define MODE -1
 
-#define BUFSIZE (128)
-
-Adafruit_BLE_SWUART ble(RX, TX, CTS, RTS, MODE);
+#define BUFSIZE 128
+#define VERBOSE_MODE true
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+SoftwareSerial bluefruitSS = SoftwareSerial(TX, RX);
+Adafruit_BluefruitLE_UART ble(bluefruitSS, MODE, CTS, RTS);
 
 void setup() {
   
   Serial.begin(115200);
-  Serial.println(F("Hand Saw Coach Starting"));
+  Serial.println(F("BLE Starting"));
   
   delay(5000);
 
-  if (! ble.begin()) {
+  if (! ble.begin(VERBOSE_MODE)) {
     Serial.println( F("FAILED!") );
     while(1);
   }
   
   Serial.println( F("OK!") );
-
-  Serial.print( F("Command mode: ") );
-  if(! ble.setModePin(BLUEFRUIT_MODE_COMMAND)) {
-    Serial.println(F("FAILED."));
-    while(1);
-  }
-
-  Serial.println( F("OK!") );
-
+  
   Serial.print(F("Factory reset: "));
   if(! ble.factoryReset()) {
     Serial.println(F("FAILED."));
     while(1);
   }
-    
+  
   Serial.println( F("OK!") );
 
   ble.echo(false);
-  
-  delay(1000);
 
   Serial.print(F("Set device name: "));
-  ble.println("AT+GAPDEVNAME=BNOIOS");
-  
-  if(! ble.waitForOK()) {
+  if(! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=BNOIOS"))) {
     Serial.println(F("FAILED."));
     while(1);  
   }
   
   Serial.println( F("OK!") );
-    
-  Serial.print( F("Switching to data mode: ") );
-  if(! ble.setModePin(BLUEFRUIT_MODE_DATA)) {
-    while(1);
-  }
   
-  Serial.println(F("OK!"));
+  ble.reset();
   
   Serial.print(F("BNO055 init: "));
   if(! bno.begin()) {
@@ -76,7 +62,17 @@ void setup() {
   Serial.println(F("OK!"));
 
   bno.setExtCrystalUse(true);
-
+  
+  while (! ble.isConnected()) {
+    delay(500);
+  }
+  
+  Serial.println(F("Connected"));
+  
+  ble.println("+++");
+  delay(100);
+  ble.waitForOK();
+  
 }
 
 void loop() {
