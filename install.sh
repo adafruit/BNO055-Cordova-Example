2cd1d955359cf878afb38cb76d626cc576acdc56
Todd Treece <toddtreece@gmail.com>
2015-05-15 18:53:52 -0400

    adds install shell script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cordova plugin add cordova-plugin-whitelist@1
+cordova plugin add com.megster.cordova.bluetoothserial
+cordova plugin add https://github.com/apache/cordova-plugin-splashscreen.git
+cordova plugin add https://github.com/EddyVerbruggen/Insomnia-PhoneGap-Plugin.git
+cordova plugin add cordova-plugin-console
+cordova platform add ios
