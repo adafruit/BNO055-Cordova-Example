9f1776c48d221be11a2a059f777f1a6a107add46
Todd Treece <toddtreece@gmail.com>
2015-06-26 14:09:27 -0400

    fixes cordova plugin error

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 npm install -g cordova
-cordova plugin add cordova-plugin-whitelist@1
-cordova plugin add com.megster.cordova.bluetoothserial
+rm -rf plugins
+rm -rf platforms
+cordova plugin add cordova-plugin-whitelist@1.0.0
+cordova plugin add org.apache.cordova.dialogs
+cordova plugin add https://github.com/toddtreece/BluetoothSerial
 cordova plugin add https://github.com/apache/cordova-plugin-splashscreen.git
 cordova plugin add https://github.com/EddyVerbruggen/Insomnia-PhoneGap-Plugin.git
 cordova plugin add cordova-plugin-console
