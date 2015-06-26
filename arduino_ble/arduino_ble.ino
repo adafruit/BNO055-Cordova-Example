dd3fc99147bd41ce43bd848845e8d8f2cafc80f5
Todd Treece <toddtreece@gmail.com>
2015-06-26 14:08:58 -0400

    adds battery info to arduino sketch

--- arduino_ble/arduino_ble.ino
+++ arduino_ble/arduino_ble.ino
@@ -14,31 +14,34 @@
 #define MODE -1
 
 #define BUFSIZE 128
-#define VERBOSE_MODE true
+#define VERBOSE_MODE false
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 SoftwareSerial bluefruitSS = SoftwareSerial(TX, RX);
 Adafruit_BluefruitLE_UART ble(bluefruitSS, MODE, CTS, RTS);
 
+int battery = 0;
+int count = 0;
+
 void setup() {
-  
+
   Serial.begin(115200);
   Serial.println(F("BLE Starting"));
-  
+
   delay(5000);
 
   if (! ble.begin(VERBOSE_MODE)) {
     Serial.println( F("FAILED!") );
     while(1);
   }
-  
+
   Serial.println( F("OK!") );
-  
+
   Serial.print(F("Factory reset: "));
   if(! ble.factoryReset()) {
     Serial.println(F("FAILED."));
     while(1);
   }
-  
+
   Serial.println( F("OK!") );
 
   ble.echo(false);
@@ -46,44 +49,68 @@ void setup() {
   Serial.print(F("Set device name: "));
   if(! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=BNOIOS"))) {
     Serial.println(F("FAILED."));
-    while(1);  
+    while(1);
   }
-  
-  Serial.println( F("OK!") );
-  
+
+  Serial.println(F("OK!"));
+
   ble.reset();
-  
+
   Serial.print(F("BNO055 init: "));
   if(! bno.begin()) {
     Serial.println(F("FAILED."));
     while(1);
   }
-   
+
   Serial.println(F("OK!"));
 
   bno.setExtCrystalUse(true);
-  
-  while (! ble.isConnected()) {
+
+  while (! ble.isConnected())
     delay(500);
-  }
-  
+
   Serial.println(F("Connected"));
-  
-  ble.println("+++");
-  delay(100);
-  ble.waitForOK();
-  
+
+  batteryLevel();
+
 }
 
 void loop() {
 
-  sensors_event_t event; 
+  sensors_event_t event;
   bno.getEvent(&event);
-  
-  ble.print(event.orientation.x, 4);
+
+  ble.print("AT+BLEUARTTX=");
+  ble.print(event.orientation.x, 1);
   ble.print(",");
-  ble.println(event.orientation.z, 4);
-  
-  delay(200);
+  ble.print(event.orientation.y, 1);
+  ble.print(",");
+  ble.print(event.orientation.z, 1);
+  ble.print(",");
+  ble.print(battery, DEC);
+  ble.println("|");
+  ble.readline(200);
+
+  if(count == 5000) {
+    batteryLevel();
+    count = 0;
+  } else {
+    delay(200);
+    count++;
+  }
+
+}
+
+void batteryLevel() {
+
+  ble.println("AT+HWVBAT");
+  ble.readline(1000);
+
+  if(strcmp(ble.buffer, "OK") == 0) {
+    battery = 0;
+  } else {
+    battery = atoi(ble.buffer);
+    ble.waitForOK();
+  }
 
 }
